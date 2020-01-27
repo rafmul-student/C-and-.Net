4505a114ef653f94d8b5f127891df7b788cd4067
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-01-27 16:01:02 +0100

    Create lab_12_5.cs

---
+++ lab_12/lab_12_5.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading;
+
+namespace Cwiczenie_5
+{
+    class ThreadExample
+    {
+        static readonly object locker = new object();
+        static bool done;
+
+        static void Main(string[] args)
+        {
+            new Thread(Run).Start();
+            Run();
+        }
+
+        static void Run()
+        {
+            lock (locker)
+            {
+                if (!done)
+                {
+                    Console.WriteLine("Done");
+                    done = true;
+                }
+            }
+        }
+    }
+}
