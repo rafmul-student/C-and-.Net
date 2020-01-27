2a84c6ad9f8a7a8d2efabd9aef284c514e1376a3
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-01-27 15:35:18 +0100

    Create lab_12_2.cs

---
+++ lab_12/lab_12_2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+
+namespace Cwiczenie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            new Thread (Run).Start();    
+            Run();
+        }
+
+        static void Run()
+        {
+            for(int cycles = 0; cycles < 5; cycles++)
+            {
+                Console.Write('x');
+            }
+        }
+    }
+}
+
+
+// Wypisywanie odbywa się 10 razy, bo funkcja Run() wywoływana jest dwukrotnie
