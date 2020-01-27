8f42990580577ce5a25bf3920b6ee569e6d9b27e
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-01-27 15:15:31 +0100

    Create lab_13_3.cs

---
+++ lab_13/lab_13_3.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task.Factory.StartNew(Run);
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Hello !! The thread pool!");
+        }
+    }
+}
+
+// Nie zostało nic wyświetlone.
