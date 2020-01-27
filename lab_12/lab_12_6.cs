48af4f63b9983339de84b0442ad6211d7ab0f567
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-01-27 16:05:36 +0100

    Create lab_12_6.cs

---
+++ lab_12/lab_12_6.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading;
+
+namespace Cwiczenie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Run);
+            t.Start();        
+            Console.WriteLine("Thread t has ended");
+        }
+
+        static void Run()
+        {
+            for(int i = 0; i < 777; i++)
+            {
+                Console.Write("[]");
+            }
+        }
+    }
+}
+
+// Bez funkcji Join() progra, od razu wypisuje "Thread t has ended" nie czekając na wykonanie wątku
