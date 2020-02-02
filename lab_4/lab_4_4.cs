941ff4e0f50439420d3990464eab570f32dab68e
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-02 23:54:04 +0100

    Create lab_4_4.cs

---
+++ lab_4/lab_4_4.cs
@@ -0,0 +1,34 @@
+using System;
+namespace lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[5];
+            string input;
+            bool isError = false;
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Console.Write("Podaj {0} liczbę typu int: ", i + 1);
+                input = Console.ReadLine();
+                if(!int.TryParse(input, out tablica[i]))
+                {
+                    Console.WriteLine("To nie jest int :(");
+                    isError = true;
+                    break;
+                }
+            }
+            if (!isError)
+            {
+                Console.WriteLine("Liczby:");
+                for (int i = 4; i >= 0; i--)
+                {
+                    Console.WriteLine(tablica[i]);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
