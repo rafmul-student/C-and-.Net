72145e73d77d673151e702bc0ac705a1265d1e3e
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-02 22:00:30 +0100

    Create lab_5_2.cs

---
+++ lab_5/lab_5_2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 i = 23;
+            object o = i;
+            i = 123;
+            Console.WriteLine(i + ", " + (Int32) o);
+            long j = (long)o;
+            Console.WriteLine(j + ", " + (Int32)o);
+        }
+    }
+}
