ca943ecb1752295923a793eb5b59fe6f0aabf6d5
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-02 20:15:34 +0100

    Create lab_6_5.cs

---
+++ lab_6/lab_6_5.cs
@@ -0,0 +1,36 @@
+using System;
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeA A = new HomeA(20);
+            Console.WriteLine(A.Price());
+        }
+
+        abstract public class Home
+        {
+            protected int price;
+            public Home(int _price)
+            {
+                this.price = _price;
+            }
+            abstract public void SetPrice(int _price);
+            abstract public int Price();
+        }
+        public class HomeA : Home
+        {
+            public HomeA(int _price) : base(_price)
+            {           }
+            override public void SetPrice(int _price)
+            {
+                this.price = _price;
+            }
+            override public int Price()
+            {
+                return this.price;
+            }
+        }
+    }
+}
