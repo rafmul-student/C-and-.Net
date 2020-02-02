6913965d44b08289dd0c322e4a93fc5f2f2cd035
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-02 22:03:08 +0100

    Create lab_5_3.cs

---
+++ lab_5/lab_5_3.cs
@@ -0,0 +1,22 @@
+using System;
+namespace lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> zm;
+            //Console.WriteLine(zm);                         // Nie udało się
+            //Console.WriteLine(zm.GetValueOrDefault());     // Nie udało się
+            //Console.WriteLine(zm.HasValue);                // Nie udało się
+            zm = 3;
+            Console.WriteLine(zm);                       
+            Console.WriteLine(zm.GetValueOrDefault());   
+            Console.WriteLine(zm.HasValue);
+            zm = null;
+            Console.WriteLine(zm);
+            Console.WriteLine(zm.GetValueOrDefault());
+            Console.WriteLine(zm.HasValue);
+        }
+    }
+}
