dfdde033d47a6544fcb6d8f576f95347dceedb52
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-02 19:55:11 +0100

    Create lab_10_3.cs

---
+++ lab_10/lab_10_3.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+namespace Cwiczenie_3
+{
+    class DisposeObject : IDisposable
+    {
+        private bool disposed = false;              
+        private IntPtr handle;                         
+        private Component component = new Component();
+        public extern static bool CloseHandle(IntPtr handle);
+        public DisposeObject(IntPtr _handle)
+        {
+            this.handle = _handle;
+        }
+        ~DisposeObject()         
+        {
+            Dispose(false);
+        }
+        public void Dispose()  
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this); 
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)    
+            {
+                if (disposing)
+                {
+                    component.Dispose(); 
+                }
+                Marshal.FreeHGlobal(handle); 
+                handle = IntPtr.Zero;
+            }
+            disposed = true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            IntPtr p = Marshal.AllocHGlobal(sizeof(int));
+            Marshal.WriteInt32(p, n);
+            int n1 = Marshal.ReadInt32(p);
+            Console.WriteLine("n = {0}\nn1 = {1}", n, n1);
+            DisposeObject dispose = new DisposeObject(p);
+            dispose.Dispose();
+        }
+    }
+}
