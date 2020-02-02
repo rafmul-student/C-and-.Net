7ebd5d469e0421d0e3756d1128f2728cc13be7d2
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-03 00:08:30 +0100

    Create lab_3_10.cs

---
+++ lab_3/lab_3_10.cs
@@ -0,0 +1,26 @@
+using System;
+namespace lab_10
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 10");
+            short sh = 1;
+            ushort ush = 2;
+            int i = 3;
+            uint ui = 4;
+            long l = 5;
+            //niejawnea
+            i = ush;
+            l = ui;
+            l = ush;
+            ui = ush;
+            //jawna
+            sh = (short)l;
+            i = (int)l;
+            sh = (short)l;
+            ush = (ushort)l;
+        }
+    }
+}
