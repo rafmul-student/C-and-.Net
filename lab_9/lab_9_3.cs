05965639cc87ee66968d12daa65b454b1cef4401
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-02 20:06:12 +0100

    Create lab_9_3.cs

---
+++ lab_9/lab_9_3.cs
@@ -0,0 +1,39 @@
+using System;
+namespace Cwiczenie_3
+{
+    class Program
+    {
+        delegate float delegata(int n1, int n2);
+
+        static void Main(string[] args)
+        {
+            delegata d = new delegata(dodaj);
+            d += new delegata(odejm);
+            d += new delegata(podziel);
+            d += new delegata(pomnoz);
+            int n1 = 4;
+            int n2 = 10;
+            d(n1, n2);
+        }
+        public static float dodaj(int n1, int n2)
+        {
+            Console.WriteLine(n1 + n2);
+            return n1 + n2;
+        }
+        public static float odejm(int n1, int n2)
+        {
+            Console.WriteLine(n1 - n2);
+            return n1 - n2;
+        }
+        public static float pomnoz(int n1, int n2)
+        {
+            Console.WriteLine(n1 * n2);
+            return n1 * n2;
+        }
+        public static float podziel(int n1, int n2)
+        {
+            Console.WriteLine(n1 / n2);
+            return n1 / n2;
+        }
+    }
+}
