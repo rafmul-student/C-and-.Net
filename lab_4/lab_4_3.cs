828ab561d5e79db5c1d4dd822d153395973db0d3
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-03 00:01:09 +0100

    Create lab_4_3.cs

---
+++ lab_4/lab_4_3.cs
@@ -0,0 +1,34 @@
+using System;
+namespace lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[10];
+            int n;
+            string input;
+            Console.Write("\nPodaj liczbę: ");
+            while ((input = Console.ReadLine()) != "q")
+            {
+                if (!int.TryParse(input, out n))
+                {
+                    Console.WriteLine("To nie jest int :( ");
+                    Console.Write("\nPodaj liczbę typu int: ");
+                    continue;
+                }
+                for (int i=8; i>=0; i--)
+                {
+                    tablica[i + 1] = tablica[i];
+                }
+                tablica[0] = n;
+                for(int i=0; i<10; i++)
+                {
+                    Console.Write("[{0}]", tablica[i]);
+                }
+                Console.WriteLine();
+                Console.Write("\nPodaj liczbę: ");
+            }            
+        }
+    }
+}
