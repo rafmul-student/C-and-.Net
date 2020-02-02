90103e0cbdb596f63431bc33bcf59fcf5beaa059
rafmul-student <56521400+rafmul-student@users.noreply.github.com>
2020-02-03 00:36:01 +0100

    Create lab_3_7.cs

---
+++ lab_3/lab_3_7.cs
@@ -0,0 +1,26 @@
+using System;
+namespace lab_7
+{
+    class Program
+    {
+        static int maxIntValue = 2147483647;
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("\nPrzepełnienie");
+            n = maxIntValue;
+            Console.WriteLine(n);
+            n += 110;
+            Console.WriteLine(n);
+            Console.WriteLine();
+            try
+            {
+                n = checked(maxIntValue + 11);
+            }
+            catch (System.OverflowException e)
+            {
+                Console.WriteLine("Błąd przepełnienia.");
+            }
+        }
+    }
+}
